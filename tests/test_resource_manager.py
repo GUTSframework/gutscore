2e43db7239196a1eca9e2ecd1270255ed7ec5dd6
Lucas Esclapez <lesclapez@esciencecenter.nl>
2025-01-06 23:54:17 +0100

    Add a couple more tests.

--- tests/test_resource_manager.py
+++ tests/test_resource_manager.py
@@ -1,4 +1,5 @@
 """Tests for the scheduler.resource_manager class."""
+import json
 import pytest
 from scheduler.resource_manager import ResourceManager
 from scheduler.sys_utils import get_cpu_count
@@ -23,6 +24,32 @@ def test_oversubscribe_resources():
     manager = ResourceManager(config)
     res_config = {"nworkers": get_cpu_count() + 1}
     with pytest.raises(RuntimeError):
+        _ = manager.get_resources(res_config, 0)
+
+def test_get_resource_local():
+    """Test requesting a local resource set from manager."""
+    config = {"resource" : {"nwgroups": 1}}
+    manager = ResourceManager(config)
+    res_config = {"nworkers": 1}
+    local_res = manager.get_resources(res_config, 0)
+    assert json.loads(local_res.serialize())["wgroup_CLI_pid"] != -1
+
+@pytest.mark.usefixtures("slurm_available")
+def test_get_resource_slurm():
+    """Test requesting a slurm resource set from manager."""
+    config = {"resource" : {"nwgroups": 1, "backend" : "slurm"}}
+    manager = ResourceManager(config)
+    res_config = {"nworkers": 1, "nodes": 1, "runtime": "1:30"}
+    slurm_res = manager.get_resources(res_config, 0)
+    assert json.loads(slurm_res.serialize())["job_id"] is not None
+
+@pytest.mark.usefixtures("slurm_available")
+def test_oversubscribe_resources_slurm():
+    """Test oversubscribing a slurm with a large number of nodes."""
+    config = {"resource" : {"nwgroups": 1, "backend" : "slurm"}}
+    manager = ResourceManager(config)
+    res_config = {"nworkers": 12, "nodes": 100000, "runtime": "1:30"}
+    with pytest.raises(ValueError):
         _ = manager.get_resources(res_config, 0)
 
 @pytest.mark.usefixtures("slurm_not_available")
